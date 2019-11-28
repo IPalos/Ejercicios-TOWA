305e839047a85b2a27a93275d131e0154b2b432b
Curso5 <ignaciopalos.r@gmail.com>
2019-11-28 10:59:13 -0600

    Fixed Credentials example

--- Dia4/app.js
+++ Dia4/app.js
@@ -9,15 +9,15 @@ body.appendChild(el);
 var card = document.createElement('div');
 body.appendChild(card);
 
-var name = document.createElement('p');
-name.innerText ='Name: ';
+var name = document.createElement('div');
+name.innerHTML ='Name: <span id="name">...</span>';
 card.appendChild(name);
 
-var username= document.createElement('p');
-username.innerText='Username: ';
+var username= document.createElement('div');
+username.innerHTML ='Username: <span id="username">...</span>';
 card.appendChild(username);
 
-var email = document.createElement('p');
-email.innerText='Email: ';
+var email = document.createElement('div');
+email.innerHTML ='Email: <span id="email">...</span>';
 card.appendChild(email);
 
