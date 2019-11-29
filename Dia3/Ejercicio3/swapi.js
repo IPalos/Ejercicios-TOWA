7903966d11bee7eace038441f2821b9ba7461943
IPalos <IPalos@MacBook-Pro-de-Hey.local>
2019-11-29 00:23:39 -0600

    Fixed Ejercicio3

--- Dia3/Ejercicio3/swapi.js
+++ Dia3/Ejercicio3/swapi.js
@@ -1,12 +1,13 @@
 var App = function(){
   console.log('inside App');
 }
-
 App.prototype.getallPeople = function(){
-  url = 'https://swapi.co/api/people'
-  
-  //consume aqui el servicio https://swapi.co/api/people/ y regresa un array de todos los nombres de las personas que tengan una altura mayor a 120
-e};
+  url = 'https://swapi.co/api/people';
+  fetch(url)
+    .then(response => response.json()
+      .then(result => console.log(result.results.filter(tall => tall.height > 120).map(p=> p.name))));
+
+};
 
 const app = new App();
 console.log('names')
